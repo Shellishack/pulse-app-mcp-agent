e9119a0ddd7f5854694e73a6c5315576a7df856f
Shellishack <40737228+Shellishack@users.noreply.github.com>
2025-11-16 07:07:02 +0800

    Change visibility

--- pulse.config.ts
+++ pulse.config.ts
@@ -15,7 +15,7 @@ const config: AppConfig = {
   displayName: packageJson.displayName,
   description: packageJson.description,
   appType: AppTypeEnum.FileView,
-  visibility: "unlisted",
+  visibility: "public",
   recommendedHeight: 640,
   recommendedWidth: 360,
   thumbnail: "assets/thumbnail.png",
